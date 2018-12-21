baa991e4ea4288de13493f8e68f48633e3f49598
Vishal Kancharla <vishalkancharla1234@gmail.com>
2018-12-22 00:09:19 +0530

    Add files via upload

---
+++ Algorithmic ToolBox/Week-1/max_pairwise_product.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main()
+{
+   int n,i;
+   cin>>n;
+   cout<<endl;
+   int64_t a[n];
+   for(i=0;i<n;i++)
+    cin>>a[i];
+    sort(a,a+n);
+   int64_t result = a[n-2]*a[n-1];
+   cout<<result<<endl;
+   return 0;
+}
